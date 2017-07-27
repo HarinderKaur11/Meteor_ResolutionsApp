0ebc0caa73551382404229d2999f8254e129a7ef
Harinder Kaur <snazyahuja11@gmail.com>
2017-07-27 10:24:13 -0400

    Make app more secure

--- server/main.js
+++ server/main.js
@@ -14,10 +14,18 @@ Meteor.startup(() => {
 	},
 
 	updateResolution:function(id,checked){
+		var res=Resolutions.findOne(id);
+		if(res.owner!==Meteor.userId()){
+			throw new Meteor.Error('not-authorized');
+		}
 		Resolutions.update(id,{$set:{ checked: checked} })
 	},
 
 	deleteResolution:function(id){
+		var res=Resolutions.findOne(id);
+		if(res.owner!==Meteor.userId()){
+			throw new Meteor.Error('not-authorized');
+		}
 		Resolutions.remove(id);
 	},
 
@@ -33,6 +41,11 @@ Meteor.startup(() => {
 });
 
 Meteor.publish("resolutions", function(){
-	return Resolutions.find();
+	return Resolutions.find({
+		$or:[
+			{private: {$ne: true}},
+			{owner: this.userId}
+		]
+	});
 
 });
